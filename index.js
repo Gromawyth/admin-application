c5f1af8ca1c63087726af7e895f1d99ec0d18ce6
Gromawyth <balazsbaliga1@gmail.com>
2026-04-11 18:48:23 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-require("./embedstudio").registerEmbedStudio(client);
 const express = require("express");
 const cors = require("cors");
 const {
@@ -74,6 +73,8 @@ const ideaSystem = require("./otletek");
 const aiModeration = require("./aimoderation");
 const systemPanel = require("./systempanel");
 const { getState } = require("./systempanel");
+const embedStudio = require("./embedstudio");
+embedStudio.registerEmbedStudio(client);
 systemPanel.registerSystemPanel(client);
 bugReport.registerBugReport(client);
 ideaSystem.registerIdeaSystem(client);
